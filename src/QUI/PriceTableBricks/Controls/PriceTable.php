6b0eb8da494c3f38a9e1d88b6ca104ce8082e08d
michael.daniel <michael.danielczok@gmail.com>
2018-02-09 13:54:23 +0100

    feat: Erstes Design ist fertig (Standard - einfaches Template). feat: Highlight Setting, für hervorgehobenes Eintrag.

--- src/QUI/PriceTableBricks/Controls/PriceTable.php
+++ src/QUI/PriceTableBricks/Controls/PriceTable.php
@@ -26,9 +26,11 @@ class PriceTable extends Control
     {
         // default options
         $this->setAttributes(array(
-            'class'   => 'qui-pricetable',
-            'entries' => array(),
-            'display' => 'default'
+            'class'                   => 'qui-pricetable',
+            'nodeName'                => 'section',
+            'inherit-template-colors' => true,
+            'entries'                 => array(),
+            'display'                 => 'default'
         ));
 
         parent::__construct($params);
@@ -41,8 +43,11 @@ class PriceTable extends Control
      */
     public function getBody()
     {
+        $this->testData();
+
         $Engine  = QUI::getTemplateManager()->getEngine();
         $entries = $this->getAttribute('entries');
+        $siteLang    = $this->getSite()->getProject()->getLang();
 
         if (is_string($entries)) {
             $entries = json_decode($entries, true);
@@ -56,29 +61,76 @@ class PriceTable extends Control
                 break;
         }
 
+        if (!$this->getAttribute('inherit-template-colors')) {
+            $noInheritTemplateColors = 'no-inherit-template-colors';
+        }
+
         $Engine->assign(array(
-            'this'    => $this,
-            'entries' => $entries
+            'this'                    => $this,
+            'entries'                 => $entries,
+            'noInheritTemplateColors' => 'no-inherit-template-colors'
         ));
 
-        $this->addCSSFile(dirname(__FILE__) . $css);
+        $this->addCSSFile($css);
 
-        return $Engine->fetch(dirname(__FILE__) . $template);
+        return $Engine->fetch($template);
     }
 
     public function testData()
     {
 
         $entries = array(
-            1 => array(
+            0 => array(
                 'title'    => 'Super Paket',
-                'subtitle' => 'Man muss ihn kaufen',
+                'subtitle' => 'Billig aber gut',
                 'price'    => '25,99',
                 'srp'      => '39,99',
-                'url'      => '#'
+                'img'      => '',
+                'url'      => '#',
+                'features' => '[{"1":"Nulla porttitor accumsan","2":"Vestibulum ac diam sit amet quam", "3":"Lorem ipsum dolor"}]'
+            ),
+            1 => array(
+                'title'    => 'Mega',
+                'subtitle' => 'Lohnt sich',
+                'price'    => '99,99',
+                'srp'      => '',
+                'img'      => '',
+                'url'      => '#',
+                'features' => '[{"1":"Quisque velit nisi","2":"Curabitur aliquet", "3":"Vestibulum ante ipsum"}]'
+            ),
+            2 => array(
+                'title'    => 'Lupsim lerum',
+                'subtitle' => 'Donec sollicitudin',
+                'price'    => '500',
+                'srp'      => '501',
+                'img'      => '',
+                'url'      => '#',
+                'features' => '[{"1":"Curabitur arcu erat","2":"Quisque velit nisi", "3":"Donec sollicitudin"}]'
             )
         );
 
+        $entries = '[{"title":"Basic","subtitle":"Perfect for small companies","price":"29,99","srp":"39,99","priceinfo":"","img":"","url":"#","highlight":"0","features":{"1":"Nulla porttitor accumsan","2":"Vestibulum ac diam sit amet quam", "3":"Lorem ipsum dolor"}},';
+        $entries .= '{"title":"Standard","subtitle":"Perfect for medium budget","price":"99,99","srp":"","priceinfo":"pro Monat","img":"","url":"#","highlight":"1","features":{"1":"Quisque velit nisi","2":"Curabitur aliquet", "3":"Vestibulum ante ipsum"}},';
+        $entries .= '{"title":"Advanced","subtitle":"Perfect for large budget","price":"500","srp":"501","priceinfo":"pro Monat","img":"","url":"#","highlight":"0","features":{"1":"Curabitur arcu erat","2":"Quisque velit nisi", "3":"Donec sollicitudin"}},';
+        $entries .= '{"title":"Advanced","subtitle":"Perfect for large budget","price":"500","srp":"501","priceinfo":"pro Monat","img":"","url":"#","highlight":"0","features":{"1":"Curabitur arcu erat","2":"Quisque velit nisi", "3":"Donec sollicitudin"}}]';
+
+
         $this->setAttribute('entries', $entries);
+    }
+
+    /**
+     * @return mixed|QUI\Projects\Site
+     */
+    protected function getSite()
+    {
+        if ($this->getAttribute('Site')) {
+            return $this->getAttribute('Site');
+        }
+
+        $Site = QUI::getRewrite()->getSite();
+
+        $this->setAttribute('Site', $Site);
+
+        return $Site;
     }
 }
